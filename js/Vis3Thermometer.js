44a84999d9484b3f753668a8f98fbc7e2837bc28
filipwodnicki <filip.wodnicki@gmail.com>
2023-12-10 23:19:16 +0100

    Connect Vis 3 Thermometer to Vis 3 Map

--- js/Vis3Thermometer.js
+++ js/Vis3Thermometer.js
@@ -5,6 +5,8 @@ class Vis3Thermometer {
     constructor(parentElement, warmingData) {
         this.parentElement = parentElement;
         this.warmingData = warmingData;
+        this.date = moment(vis3MapDate.innerText, "MMM YYYY").format("YYYY-MM-DD"); // Dependent on MapVis Slider Date
+        this.warmingRow = null;
 
         // define colors
         this.colors = ['#fddbc7', '#f4a582', '#d6604d', '#b2182b']
@@ -25,6 +27,19 @@ class Vis3Thermometer {
             .attr("height", vis.height)
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
+        // Format the data
+        vis.warmingData.forEach(d => {
+            d.projected_warming = +d.projected_warming;
+            d.actual_warming = +d.actual_warming;
+        })
+
+        // Get the initial row
+        vis.warmingRow = vis.warmingData.filter(d => d.date === vis.date)[0];
+        console.log(vis.warmingRow)
+
+        // Get data
+        console.log("vis3MapDate:", vis.date);
+
         // // add title
         // vis.svg.append('g')
         //     .attr('class', 'title')
@@ -34,6 +49,12 @@ class Vis3Thermometer {
         //     .attr('transform', `translate(${vis.width / 2}, 20)`)
         //     .attr('text-anchor', 'middle');
 
+
+        // Define additional listener on the Map SLider
+        vis3MapSlider.noUiSlider.on('update', function (values) {
+            vis.wrangleData();
+        });
+
         vis.wrangleData()
 
     }
@@ -41,12 +62,19 @@ class Vis3Thermometer {
     wrangleData() {
         let vis = this;
 
+        // Update the vis date
+        this.date = moment(vis3MapDate.innerText, "MMM YYYY").format("YYYY-MM-DD");
+
+        // Get the new row
+        vis.warmingRow = vis.warmingData.filter(d => d.date === vis.date)[0];
+        console.log("New row:", vis.warmingRow)
+
         vis.updateVis()
     }
 
     updateVis() {
         let vis = this;
 
-
     }
 }
+
